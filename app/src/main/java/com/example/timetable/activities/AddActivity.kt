6e89a201769c64672e4365d380cd11cbfbb18b47
Keixxd <mmoboomkin.gg@mail.ru>
2021-10-29 01:48:55 +0300

    some AddActivity ui advancements and batch mode feature release

--- app/src/main/java/com/example/timetable/activities/AddActivity.kt
+++ app/src/main/java/com/example/timetable/activities/AddActivity.kt
@@ -2,8 +2,10 @@ package com.example.timetable.activities
 
 import android.app.TimePickerDialog
 import android.os.Bundle
+import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
+import android.widget.EditText
 import android.widget.TextView
 import android.widget.Toast
 import androidx.activity.viewModels
@@ -57,29 +59,55 @@ class AddActivity : AppCompatActivity() {
     }
 
     private fun insertJobToDatabase() {
-        val jobName = binding.addJobName.text.toString()
-        val jobTeacher = binding.addJobTeacher.text.toString()
-        val jobClass = binding.addJobClass.text.toString()
-        val jobStartTime = binding.startTimeText.text.toString()
-        val jobEndTime = binding.endTimeText.text.toString()
-        val jobType = itemsList[binding.jobTypePicker.selectedTabPosition]
-        val dayOfWeek = dayMap[binding.dayPicker.selectedTabPosition]
-
-        if (isInputValid(jobName, jobTeacher, jobStartTime, jobEndTime, jobClass)) {
-            val job = Job(null, dayOfWeek, jobName, jobTeacher, jobStartTime, jobEndTime, jobClass, jobType)
-            viewModel.addJob(job)
-            onBackPressed()
-        }else{
-            Toast.makeText(this, "Заполните пустые поля", Toast.LENGTH_SHORT).show()
+        with(binding) {
+            if(isInputValid(
+                addJobName.text.toString(),
+                addJobTeacher.text.toString(),
+                addJobClass.text.toString()
+            )){
+                viewModel.addJob(Job(
+                    null,
+                    dayMap[dayPicker.selectedTabPosition],
+                    addJobName.text.toString(),
+                    addJobTeacher.text.toString(),
+                    startTimeText.text.toString(),
+                    endTimeText.text.toString(),
+                    addJobClass.text.toString(),
+                    itemsList[binding.jobTypePicker.selectedTabPosition]
+                ))
+                // batch mode check
+                if(!batchSwitch.isChecked){
+                    onBackPressed()
+                    finish()
+                }
+            }else{
+                showErrorDrawables()
+                Toast.makeText(this@AddActivity, "Заполните пустые поля", Toast.LENGTH_LONG).show()
+            }
         }
     }
 
-    private fun isInputValid(jobName: String, jobTeacher: String,
-                             jobStartTime: String, jobEndTime: String, jobClass: String): Boolean {
+    private fun showErrorDrawables(){
+        with(binding) {
 
-        return !(jobName.isEmpty() && jobTeacher.isEmpty()
-                && jobStartTime.isEmpty() && jobClass.isEmpty() && jobEndTime.isEmpty() )
+            val checkState: (EditText) -> Unit = { et ->
+                if(et.text.isBlank())
+                    et.setCompoundDrawablesRelativeWithIntrinsicBounds(0,0,R.drawable.ic_baseline_error_24, 0)
+                else
+                    et.setCompoundDrawablesRelativeWithIntrinsicBounds(0,0,0, 0)
+            }
+
+            checkState(addJobName)
+            checkState(addJobTeacher)
+            checkState(addJobClass)
+        }
     }
+
+    private fun isInputValid(jobName: String,
+                             jobTeacher: String,
+                             jobClass: String
+    ): Boolean = !(jobName.isBlank() || jobTeacher.isBlank() || jobClass.isBlank())
+
 
     private fun initTimePickers(pickerCard: CardView, timeTextView: TextView){
 
@@ -116,5 +144,10 @@ class AddActivity : AppCompatActivity() {
     override fun onSupportNavigateUp(): Boolean {
         onBackPressed()
         return true
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d("info_log", "onDestroy")
     }
 }
